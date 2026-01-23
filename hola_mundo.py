c6a84d57b9f73f72ea1d0c872932a7fd68030550
Simón Osorio <simon.osorioe@upb.edu.co>
2026-01-23 15:07:48 -0500

    Al archivo hola_mundo.py se le agrega un código para calcular números primos

--- hola_mundo.py
+++ hola_mundo.py
@@ -1,3 +1,20 @@
 # mi_primer_script.py
 
 print("¡Hola, Github!")
+
+def primos_hasta_1000():
+    limite = 1000
+    es_primo = [True] * (limite + 1)
+    es_primo[0] = es_primo[1] = False
+
+    for i in range(2, int(limite ** 0.5) + 1):
+        if es_primo[i]:
+            for j in range(i * i, limite + 1, i):
+                es_primo[j] = False
+
+    primos = [i for i in range(2, limite + 1) if es_primo[i]]
+    return primos
+
+# Mostrar los números primos entre 1 y 1000
+print(primos_hasta_1000())
+
